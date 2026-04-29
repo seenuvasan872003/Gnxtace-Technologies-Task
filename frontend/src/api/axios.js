e452423fd88466d1a4d651f3333231da22f57894
My Name <seenu.weboin@gmail.com>
2026-04-29 23:05:48 +0530

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:'https://gnxtace-technologies-task.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://gnxtace-technologies-task.onrender.com/api',
 });
 
 // Add token to requests
